83ce806edbaf45be47383b45e084d92a7af8b61b
constan1000 <hrairimejdi99@gmail.com>
2022-08-02 18:23:04 +0100

    0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
-
+ // Star Wars Characters
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, async (error, res) => {
-  if (error) console.log(error);
-  else {
-    const list = JSON.parse(res.body);
-    Execute(list.characters, 0);
-  }
-});
 
-const Execute = (list, index) => {
-  request.get(list[index], (error, res, body) => {
-    if (error) throw error;
-    const result = JSON.parse(body);
-    console.log(result.name);
-    if (index < list.length - 1) {
-      Execute(list, index + 1);
+request(url, async(err, response, body) => {
+    if (err) {
+        console.log(err);
     }
-  });
-};
+    for (const cId of JSON.parse(body).characters) {
+        await new Promise((resolve, reject) => {
+            request(cId, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log(JSON.parse(body).name);
+                resolve();
+            });
+        });
+    }
+});

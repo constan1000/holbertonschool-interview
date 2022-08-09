9aa37e2000bf2c96e8b0b435ff74ee17566bceb5
constan1000 <hrairimejdi99@gmail.com>
2022-08-09 11:28:46 +0100

    regex.c

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -13,13 +13,23 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-		int cheks1 = 0, checks2 = 0;
+		int checks1 = 0, checks2 = 0;
 
 	if (!str || !pattern)
 		return (0);
 
-	cheks1 = *str && (*str == *pattern || *pattern == '.');
+	checks1 = *str && (*str == *pattern || *pattern == '.');
 	checks2 = *(pattern + 1) == '*';
 
+	if (!*str && !checks2)
+		return (*pattern ? 0 : 1);
 
+	if (checks1 && checks2)
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	else if (checks1 && !checks2)
+		return (regex_match(str + 1, pattern + 1));
+	else if (checks2)
+		return (regex_match(str, pattern + 2));
+
+	return (0);
 }

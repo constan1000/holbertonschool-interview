7323e64e473427b34081a990c5995ca69996be9f
constan1000 <hrairimejdi99@gmail.com>
2022-09-22 17:36:41 +0100

    0-pascal_triangle

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -6,6 +6,6 @@ def pascal_triangle(n):
     """Computes N rows of Pascal's Triangle"""
     pascal = [[1]]
     for _ in range(n - 1):
-        pascal += [[1] + [pascal
+        pascal += [[1] + [pascal[-1][i] + pascal[-1][i + 1]
                    for i in range(len(pascal[-1]) - 1)] + [1]]
     return pascal if n > 0 else []
